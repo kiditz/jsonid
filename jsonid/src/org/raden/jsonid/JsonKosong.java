3a4ec51bf18982ad59f30321c730559360de3013
kiditz <kiditz@kiditz>
2016-09-14 00:08:43 +0700

    Remove Comment

--- jsonid/src/org/raden/jsonid/JsonKosong.java
+++ jsonid/src/org/raden/jsonid/JsonKosong.java
@@ -10,12 +10,10 @@ package org.raden.jsonid;
 public class JsonKosong extends JsonElement {
 
 	public JsonKosong() {
-		// DO NOTHING
 	}
 
 	@Override
 	protected JsonElement salin() {
 		return new JsonKosong();
 	}
-
 }
